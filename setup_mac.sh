303c2e6cc58318c18a14f79e86735121d418ee32
muraoka-edo <dice.k.edo+muraoka-edo@gmail.com>
2014-11-30 07:51:07 +0900

    add setup_mac.sh

---
+++ setup_mac.sh
@@ -0,0 +1,14 @@
+# Gnu
+brew install coreutils gnu-sed gawk
+
+# Command
+brew install tree wget
+
+# VIm
+brew install autoconf readline lua vim
+
+# rbenv
+brew install openssh install zlib libyaml ruby-build
+
+# Middleware
+brew install jenkins postgresql
