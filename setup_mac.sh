94066b3691cf59591130f5962b5bdbe9b059bd3e
muraoka-edo <dice.k.edo+muraoka-edo@gmail.com>
2014-12-21 09:49:19 +0900

    update: setup_mac.sh

--- setup_mac.sh
+++ setup_mac.sh
@@ -1,14 +1,73 @@
-# Gnu
-brew install coreutils gnu-sed gawk
+echo 'Brewfile making...'
+echo '
+# Add repositories
+tap homebrew/versions 
+tap caskroom/cask     
+tap caskroom/versions 
 
-# Command
-brew install tree wget
+# Update Homebrew
+update 
 
-# VIm
-brew install autoconf readline lua vim
+# Install formulae
+install brew-cask 
+
+# Install others...
+install wget      
+install tree
+install jq
+install ssh-copy-id
+
+# gnu
+install coreutils
+install gnu-sed
+install gawk
+
+# vim
+install autoconf
+install readline
+install lua
+install vim
 
 # rbenv
-brew install openssh install zlib libyaml ruby-build
+install openssh
+install zlib 
+install libyaml
+install ruby-build
 
 # Middleware
-brew install jenkins postgresql
+install jenkins
+install postgresql
+
+# -----------------------
+# Install Cask formulae
+cask install google-chrome 
+cask install iterm2
+
+# Java
+cask install java
+cask install java7
+
+# IDE
+cask install eclipse-ide
+cask install sourcetree
+
+# VM
+cask install virtualbox
+cask install vagrant
+cask install boot2docker
+' > Brewfile
+
+echo '
+===========================
+You must exec:
+---------------------------
+$ ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+$ brew update
+$ brew bundle
+$ echo "JAVA_HOME=$(/usr/libexec/java_home -v 1.8)" > ~/.bash_profile
+---------------------------
+note:
+http://tech.basicinc.jp/Mac/2014/04/20/mac_app_engineer/
+===========================
+'
+
